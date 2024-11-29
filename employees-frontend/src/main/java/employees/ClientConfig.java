d4dd926154f790d3a2d1daccccc884107862f621
Istvan Viczian <viczian.istvan@gmail.com>
2024-11-29 13:27:22 +0100

    Bearer token küldése

--- employees-frontend/src/main/java/employees/ClientConfig.java
+++ employees-frontend/src/main/java/employees/ClientConfig.java
@@ -4,6 +4,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -12,9 +14,14 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @EnableConfigurationProperties(EmployeesProperties.class)
 public class ClientConfig {
     @Bean
-    public EmployeesClient employeesClient(WebClient.Builder builder, EmployeesProperties employeesProperties) {
+    public EmployeesClient employeesClient(WebClient.Builder builder, EmployeesProperties employeesProperties,
+                                           OAuth2AuthorizedClientManager authorizedClientManager) {
+        var oauth2 = new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+        oauth2.setDefaultOAuth2AuthorizedClient(true);
+
         var webClient = builder
                 .baseUrl(employeesProperties.getBackendUrl())
+                .apply(oauth2.oauth2Configuration())
                 .build();
         var factory = HttpServiceProxyFactory
                 .builder(WebClientAdapter.forClient(webClient)).build();
--- employees-frontend/src/main/java/employees/SecurityConfiguration.java
+++ employees-frontend/src/main/java/employees/SecurityConfiguration.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration(proxyBeanMethods = false)
@@ -15,7 +21,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(registry ->
-                        registry.requestMatchers("/create-employee")
+                        registry.requestMatchers("/**")
                                 .authenticated()
                                 .anyRequest()
                                 .permitAll())
@@ -23,4 +29,25 @@ public class SecurityConfiguration {
                 .logout(conf -> conf.logoutSuccessUrl("/"));
         return http.build();
     }
+
+    @Bean
+    public OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+        OAuth2AuthorizedClientProvider authorizedClientProvider =
+                OAuth2AuthorizedClientProviderBuilder.builder()
+                        .authorizationCode()
+                        .refreshToken()
+                        .clientCredentials()
+                        .build();
+
+        DefaultOAuth2AuthorizedClientManager authorizedClientManager =
+                new DefaultOAuth2AuthorizedClientManager(
+                        clientRegistrationRepository, authorizedClientRepository);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        return authorizedClientManager;
+    }
+
 }
